b2ad079dee1124ff9acf6b7d9917b945af9b7848
esru13 <esraelb313@gmail.com>
2022-12-01 19:49:09 +0300

    infinte

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv) == 1):
-        print("0 arguments.")
-    elif (len(sys.argv) == 2):
-        print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
-    else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+    argc = len(sys.argv)
+    argv = sys.argv
+    res = 0
+
+    for i in range(1, argc):
+        res = res + int(argv[i])
+
+    print(res)
