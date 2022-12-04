11891b9875f0bcacb64447aaaa65c3e282b4d46f
esru13 <esraelb313@gmail.com>
2022-12-04 19:05:31 +0300

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,36 +1,33 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * is_palindrome - Return is a list s palindrome or not
- * @head:  double ponter to head
- *
- * Return: 1 on success
+ * is_palindrome - test if is a palindrome
+ * @head: firts node of list
+ * Return: 1 if is palindrome, 0 for no
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current;
-	int count, i;
-	int nums[10000];
+	listint_t *buscar;
+	int con1 = 0, con2 = 0;
+	int arr[4096];
 
-	current = *head;
-	count = 0;
 
-	if (*head == NULL)
+	if (!head)
+		return (0);
+	buscar = *head;
+	if (!*head || (*head)->next == NULL)
 		return (1);
 
-	while (current != NULL)
+	for (con1 = 0; buscar; buscar = buscar->next, con1++)
+		arr[con1] = buscar->n;
+	for (con1--; con1 > con2; con1--, con2++)
 	{
-		nums[count] = current->n;
-		current = current->next;
-		count++;
-	}
-
-	for (i = 0; i < count; i++)
-	{
-		if (nums[i] == nums[count - 1])
-			count--;
+		if (arr[con2] == arr[con1])
+			;
 		else
 			return (0);
-	} return (1);
+	}
+	return (1);
 }
