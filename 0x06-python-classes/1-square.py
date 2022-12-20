d35c28a5cad7071a4b6ded4ee0846b0b5802cb53
esru13 <esraelb313@gmail.com>
2022-12-21 00:17:55 +0300

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Creates an empty class called Square
+"""
+
+
+class Square:
+    """
+    Class definition: Square
+    Attributes:
+    __size (int): square size private
+    """
+    def __init__(self, size):
+        """
+        Args:
+            size: swuare size
+        """
+        self.__size =
