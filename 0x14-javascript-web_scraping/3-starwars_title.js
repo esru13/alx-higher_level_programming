90678cfe2c6d1ccf33890ae22fcd635e115184df
esru13 <esraelb313@gmail.com>
2023-04-25 22:59:57 +0300

    startwars api

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Star wars API
+const StarWarsAPI_URL = 'https://swapi-api.hbtn.io/api/films/:id';
+const request = require('request');
+const episodeNum = process.argv[2];
+
+
+request(StarWarsAPI_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
