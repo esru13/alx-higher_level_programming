260574a174b9737bdc0c5ca7c9d0da5de4a252d4
esru13 <esraelb313@gmail.com>
2023-04-25 23:40:38 +0300

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-require('fs').writeFile(process.argv[2], process.argv[3], 'utf-8', error=> {
-  if (error) console.log(error);
+require('fs').writeFile(process.argv[2], process.argv[3], 'utf-8', e => {
+  if (e) console.log(e);
 });
