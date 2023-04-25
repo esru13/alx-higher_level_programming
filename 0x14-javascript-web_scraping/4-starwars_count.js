05991ed8df1f45333e8da8d7e116de0d5b3d1418
esru13 <esraelb313@gmail.com>
2023-04-25 23:45:46 +0300

    count starwars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-require('request')(process.argv[2], (err, r, b) => {
-//Wedge Antilles is character ID 18
-  if (err) console.log(err);
+require('request')(process.argv[2], (e, r, b) => {
+  if (e) console.log(e);
   else {
     const nb = JSON.parse(b).results.filter((elem) => {
       return elem.characters.filter((url) => { return url.includes('18'); }).length;
