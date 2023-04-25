3a951929d1dd9c899321086e1731731229e8d577
esru13 <esraelb313@gmail.com>
2023-04-25 23:37:40 +0300

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 //file argument path
 const fs = require('fs');
-//argument is the string to write
+
 fs.writeFile(process.argv[2], process.argv[3],'utf-8', error => {
-//error occurred during while writing, print the error object
   if (error) console.log(error);
 });
