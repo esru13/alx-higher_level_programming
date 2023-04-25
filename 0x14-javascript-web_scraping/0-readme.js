5a0fda21fb121f776f75ba3c1db690841f3d879e
esru13 <esraelb313@gmail.com>
2023-04-25 23:43:02 +0300

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-//fiile argument path
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8',
-  function (err, inputD) {
-  //error occurred ,print the error. 
+fs.readFile(process.argv[2], 'utf-8', function (err, inputD) {
   console.log(err || inputD);
 });
