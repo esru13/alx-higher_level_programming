662a166e004a50201684cb7bf10b4cf785a901aa
esru13 <esraelb313@gmail.com>
2023-04-25 23:47:23 +0300

    titlefor starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
-const StarWarsAPI_URL = 'https://swapi-api.hbtn.io/api/films/';
-const request = require('request');
-const episodeNum = process.argv[2];
 
-request(StarWarsAPI_URL + episodeNum, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const responseJSON = JSON.parse(body);
-    console.log(responseJSON.title);
-  } else {
-    console.log('Error code: ' + response.statusCode);
-  }
+require('request')('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (e, r, b) => {
+  if (e) console.log(e);
+  else console.log(JSON.parse(b).title);
 });
